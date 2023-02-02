a95314a0a2e03dd30736caa653503ee227306a33
Bernardo Pinheiro <111240182+berpinheiroo@users.noreply.github.com>
2023-02-02 20:46:05 -0300

    1.6.2

--- mod2ex1.6.js
+++ mod2ex1.6.js
@@ -1,16 +1,40 @@
 // 1.6.1
 
-function confirmarDados(nome){
-     var nomeCompleto = nome + ' da Silva';
-     var idade = 20;
-     function getAnoNascimento(){
-         var ano = 2022 - idade;
-         return ano
+// function confirmarDados(nome){
+//      var nomeCompleto = nome + ' da Silva';
+//      var idade = 20;
+//      function getAnoNascimento(){
+//          var ano = 2022 - idade;
+//          return ano
+//     }
+
+//     console.log('nome completo', nomeCompleto);
+//      console.log('ano de nascimento', getAnoNascimento());
+//      console.log('idade', idade)
+//  }
+
+//  confirmarDados('Marcos');
+
+
+
+
+// 1.6.2
+
+ function confirmarDados(nome){
+    const nomeCompleto = nome + ' da Silva'
+    let geracao = 'millenium';
+    function getAnoNascimento(){
+        let idade = 40;
+        const ano = 2022 - idade;
+        return ano;
     }
 
+    let anoDeNascimento = getAnoNascimento();
+    if(anoDeNascimento <= 1995){
+         geracao = 'geração X';
+    }
     console.log('nome completo', nomeCompleto);
-     console.log('ano de nascimento', getAnoNascimento());
-     console.log('idade', idade)
+    console.log('ano de nascimento', anoDeNascimento);
+    console.log('geração', geracao)
  }
-
- confirmarDados('Marcos');
+ confirmarDados('Marcos')
