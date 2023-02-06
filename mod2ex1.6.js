51f61bfaa9377abc432ab96bb8ad06063b5c2cd0
Bernardo Pinheiro <111240182+berpinheiroo@users.noreply.github.com>
2023-02-06 19:17:28 -0300

    1.6.3

--- mod2ex1.6.js
+++ mod2ex1.6.js
@@ -20,27 +20,43 @@
 
 // 1.6.2
 
- function confirmarDados(nome){
-    const nomeCompleto = nome + ' da Silva'
-    let geracao = 'millenium';
-    function getAnoNascimento(){
-        let idade = 40;
-        const ano = 2022 - idade;
-        return ano;
-    }
+//  function confirmarDados(nome){
+//     const nomeCompleto = nome + ' da Silva'
+//     let geracao = 'millenium';
+//     function getAnoNascimento(){
+//         let idade = 40;
+//         const ano = 2022 - idade;
+//         return ano;
+//     }
 
-    let anoDeNascimento = getAnoNascimento();
-    if(anoDeNascimento <= 1995){
-         geracao = 'geração X';
-    }
-    console.log('nome completo', nomeCompleto);
-    console.log('ano de nascimento', anoDeNascimento);
-    console.log('geração', geracao)
- }
- confirmarDados('Marcos')
+//     let anoDeNascimento = getAnoNascimento();
+//     if(anoDeNascimento <= 1995){
+//          geracao = 'geração X';
+//     }
+//     console.log('nome completo', nomeCompleto);
+//     console.log('ano de nascimento', anoDeNascimento);
+//     console.log('geração', geracao)
+//  }
+//  confirmarDados('Marcos')
 
  
 
  // 1.6.3
 
- 
+ function confirmarDados(nome) {
+        try {
+            const nomeCompleto = nome + ' da Silva'
+            function getAnoNascimento() {
+                const idade = 20
+                const ano = 2022 - idade
+                return ano
+            }
+            console.log('nome completo', nomeCompleto)
+            console.log('ano de nascimento', getAnoNascimento())
+            console.log('idade', idade)
+        }catch{
+            console.log('um erro foi encontrado e não foi possível concluir a operação')
+        }
+    }
+    
+    confirmarDados('Marcos')
